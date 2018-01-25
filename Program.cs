7b8ab0ca0f750883306b118959eeedea9b1f6b14
Faithe Olson <faithe.olson@gmail.com>
2018-01-25 13:41:15 -0500

    First Comitt

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Repeat = true;
+            int Counter = 1;
+            
+            while (Repeat == true)
+            {
+                Console.WriteLine("Welcome to the Grand Circus Casino!!! Would you like to roll the dice? (y/n)");
+                string Input = (Console.ReadLine());
+                if (Input == "y" || Input == "Y")
+                {
+                    RollDice(Counter);
+                    Counter++;
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye!");
+                    Repeat = false;
+                }
+            }
+        }
+
+        public static void RollDice(int Counter)
+        {
+            
+            Console.WriteLine("How many sides should each die have?");
+            int DieSides = int.Parse(Console.ReadLine());
+
+            int Die1 = new Random().Next(1, DieSides);
+            System.Threading.Thread.Sleep(250);
+            int Die2 = new Random().Next(1, DieSides);
+
+            Console.WriteLine($"Roll #{Counter}: {Die1} & {Die2}");
+        }
+    }
+}
